cdd3a9c001aa336a17aa15e4b02807a6d26f6eeb
Rafael Mota <rma7@cin.ufpe.br>
2019-04-06 16:51:15 -0300

    Left code

--- Test.java
+++ Test.java
@@ -1,13 +1,14 @@
 class Test {
 	public void mergeConflictOne() {
-		int x = 0;
+		int x = 2;
 	}
 
 	public void mergeConflictTwo () {
-		int y = 0;
+		int y = 2;
 	}
 
 	public void noConflict () {
+		int z = 0;
 		int x = 0;
 	}
 }
